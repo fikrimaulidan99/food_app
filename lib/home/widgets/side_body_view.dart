3dfcd2b9b7aad2ef8c6b34d46a11fc2992525c6e
pradana4648 <wakwaujaim@gmail.com>
2021-08-03 10:39:42 +0700

    Remodel layout Menu

--- lib/home/widgets/side_body_view.dart
+++ lib/home/widgets/side_body_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 import '../../../icon_category.dart';
 
@@ -13,87 +14,119 @@ class SideBodyView extends StatelessWidget {
       width: size.width * 0.8,
       height: size.height,
       color: Colors.white,
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            buildTitleDescription(context),
-            buildCategoryMenu(),
-            buildBodyView()
-          ],
-        ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          buildTitleDescription(context),
+          buildCategoryMenu(),
+          buildBodyView()
+        ],
       ),
     );
   }
 
-  LimitedBox buildBodyView() {
+  Widget buildBodyView() {
     return LimitedBox(
       maxHeight: size.height * 0.6,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         physics: BouncingScrollPhysics(),
         itemBuilder: (context, index) => Container(
-          width: size.width * 0.6,
-          margin: const EdgeInsets.only(
-            left: 25,
-            top: 30,
-            right: 15,
-          ),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-              // topLeft: Radius.circular(100),
-              // topRight: Radius.circular(20),
-              bottomLeft: Radius.circular(20),
-              bottomRight: Radius.circular(20),
+          width: size.width * 0.65,
+          padding: const EdgeInsets.all(10),
+          child: Material(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
             ),
             color: Theme.of(context).canvasColor,
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(15),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Image.asset('assets/images/pancakes.png'),
-                Text(
-                  'Pancakes',
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-                Text(
-                  'with Blueberry',
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-                ListTile(
-                  contentPadding: EdgeInsets.zero,
-                  title: RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                          text: '420',
-                          style: Theme.of(context).textTheme.headline4,
+            child: InkWell(
+              onTap: () {
+                print('Pres');
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Image.asset(
+                        'assets/images/pancakes.png',
+                        // fit: BoxFit.cover,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Chip(
+                          avatar: CircleAvatar(
+                            backgroundColor: Colors.transparent,
+                            foregroundColor: Colors.black,
+                            child: Icon(Icons.timer),
+                          ),
+                          backgroundColor: Colors.transparent,
+                          label: Text('5 min'),
                         ),
-                        TextSpan(
-                          text: ' kcal',
-                          style: Theme.of(context).textTheme.headline5,
+                        Chip(
+                          avatar: CircleAvatar(
+                            backgroundColor: Colors.transparent,
+                            foregroundColor: Colors.black,
+                            child: Icon(Icons.fastfood),
+                          ),
+                          backgroundColor: Colors.transparent,
+                          label: Text('1 serve'),
                         ),
                       ],
                     ),
-                  ),
-                  trailing: Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.white,
+                    // RatingBar.builder(
+                    //   itemBuilder: (context, index) => Icon(Icons.star),
+                    //   itemSize: 30,
+                    //   updateOnDrag: true,
+                    //   allowHalfRating: true,
+                    //   onRatingUpdate: (value) {
+                    //     print(value);
+                    //   },
+                    // ),
+                    Text(
+                      'Pancakes',
+                      style: Theme.of(context).textTheme.headline4,
                     ),
-                    child: IconButton(
-                      icon: Icon(
-                        Icons.bookmark,
-                        color: Theme.of(context).iconTheme.color,
+                    Text(
+                      'with Blueberry',
+                      style: Theme.of(context).textTheme.headline5,
+                    ),
+                    ListTile(
+                      contentPadding: EdgeInsets.zero,
+                      title: RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text: '420',
+                              style: Theme.of(context).textTheme.headline4,
+                            ),
+                            TextSpan(
+                              text: ' kcal',
+                              style: Theme.of(context).textTheme.headline5,
+                            ),
+                          ],
+                        ),
                       ),
-                      onPressed: () {},
-                    ),
-                  ),
-                )
-              ],
+                      trailing: Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                        ),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.bookmark,
+                            color: Theme.of(context).iconTheme.color,
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
         ),
